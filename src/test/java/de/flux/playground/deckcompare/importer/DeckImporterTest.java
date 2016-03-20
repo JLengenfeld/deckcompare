b3c3e6012f1c2f2915a2734c582b945ebdd93ccb
JLengenfeld <jan.lengenfeld@gmail.com>
2016-03-21 00:54:16 +0100

    Add more specific asserts for the imported deck

--- src/test/java/de/flux/playground/deckcompare/importer/DeckImporterTest.java
+++ src/test/java/de/flux/playground/deckcompare/importer/DeckImporterTest.java
@@ -3,7 +3,6 @@ package de.flux.playground.deckcompare.importer;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.empty;
 
 import java.io.File;
 import java.net.URISyntaxException;
@@ -15,6 +14,9 @@ import de.flux.playground.deckcompare.dto.Deck;
 
 public class DeckImporterTest {
 
+    private static final int FIRST_SECTION = 0;
+    private static final int SECOND_SECTION = 1;
+
     @Test
     public void importDeck() throws Exception {
         DeckImporter importer = new DeckImporter();
@@ -22,9 +24,25 @@ public class DeckImporterTest {
         File deckFile = obtainDeckFile("the-fun-ends-here-shl-jinteki-edition-1st-.o8d");
         Deck deck = importer.importDeck(deckFile);
 
-        assertThat(deck.getGame(), is(not("")));
-        assertThat(deck.getSection(), is(not(empty())));
+        assertThat(deck.getGame(), is("0f38e453-26df-4c04-9d67-6d43de939c77"));
+        assertSections(deck);
         assertThat(deck.getNotes(), is(not("")));
+    }
+
+    private void assertSections(Deck deck) {
+        assertThat(deck.getSection().size(), is(2));
+        assertIdentity(deck);
+        assertCardsOfDeck(deck);
+    }
+
+    private void assertIdentity(Deck deck) {
+        assertThat(deck.getSection().get(FIRST_SECTION).getCard().size(), is(1));
+        // TODO: add more asserts for the identity
+    }
+
+    private void assertCardsOfDeck(Deck deck) {
+        assertThat(deck.getSection().get(SECOND_SECTION).getCard().size(), is(24));
+        // TODO: add more asserts for the cards of the deck
     }
 
     private File obtainDeckFile(String fileName) throws URISyntaxException {
