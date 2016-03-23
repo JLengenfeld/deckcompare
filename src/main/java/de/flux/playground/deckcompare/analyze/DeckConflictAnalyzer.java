7bf003ce6c6dc13f12e6304c9007e3b0980733be
JLengenfeld <jan.lengenfeld@gmail.com>
2016-03-24 00:29:11 +0100

    Implement method to identify conflicted cards

--- src/main/java/de/flux/playground/deckcompare/analyze/DeckConflictAnalyzer.java
+++ src/main/java/de/flux/playground/deckcompare/analyze/DeckConflictAnalyzer.java
@@ -1,14 +1,26 @@
 package de.flux.playground.deckcompare.analyze;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import de.flux.playground.deckcompare.dto.Card;
 import de.flux.playground.deckcompare.dto.Deck;
+import lombok.NonNull;
 
 public class DeckConflictAnalyzer {
 
-    public static List<Card> analyze(Deck deckA, Deck deckA2) {
-        return null;
+    public static List<Card> analyze(@NonNull Deck deckA, @NonNull Deck deckB) {
+        ArrayList<Card> conflictedCards = new ArrayList<Card>();
+
+        for (Card cardA : deckA.getCards()) {
+            for (Card cardB : deckB.getCards()) {
+                if (CardConflictAnalyzer.conflicted(cardA, cardB)) {
+                    conflictedCards.add(new Card(cardA.getId(), cardA.getValue()));
+                }
+            }
+        }
+
+        return conflictedCards;
     }
 
 }
