b99a593e72066748465116344f333b1ac927255b
JLengenfeld <jan.lengenfeld@gmail.com>
2016-03-24 23:48:21 +0100

    Add spring-boot and spring-devtools dependency
    and add a class as an entry point for SpringApplication.run(...)

---
+++ src/main/java/de/flux/playground/deckcompare/controller/Deckcompare.java
@@ -0,0 +1,23 @@
+package de.flux.playground.deckcompare.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class Deckcompare {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Deckcompare.class, args);
+    }
+
+}
