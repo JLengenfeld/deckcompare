6d5abfb019579b7d5b87dbee4abbe81465f7a878
JLengenfeld <jan.lengenfeld@gmail.com>
2016-03-24 01:30:43 +0100

    Put field to top of class

--- src/main/java/de/flux/playground/deckcompare/importer/ImportException.java
+++ src/main/java/de/flux/playground/deckcompare/importer/ImportException.java
@@ -2,10 +2,10 @@ package de.flux.playground.deckcompare.importer;
 
 public class ImportException extends RuntimeException {
 
+    private static final long serialVersionUID = 1734612217426583223L;
+
     public ImportException(String string, Throwable cause) {
         super(string, cause);
     }
-
-    private static final long serialVersionUID = 1734612217426583223L;
 
 }
