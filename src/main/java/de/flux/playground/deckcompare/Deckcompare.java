c78abe1c9d4083fde78338906a2f8f6544ccfc9d
JLengenfeld <jan.lengenfeld@gmail.com>
2016-03-26 22:58:02 +0100

    Add tyhmeleaf and file upload example form spring.io

--- src/main/java/de/flux/playground/deckcompare/Deckcompare.java
+++ src/main/java/de/flux/playground/deckcompare/Deckcompare.java
@@ -1,19 +1,32 @@
 package de.flux.playground.deckcompare;
 
+import java.io.File;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Deckcompare {
+
+    public static String ROOT = "upload-dir";
 
     @RequestMapping("/")
     @ResponseBody
     String home() {
         return "Hello World!";
+    }
+
+    @Bean
+    CommandLineRunner init() {
+        return (String[] args) -> {
+            new File(ROOT).mkdir();
+        };
     }
 
     public static void main(String[] args) throws Exception {
