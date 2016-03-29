bdf51081af17ed2d9dd157693984552eefcda70c
Benedikt Ritter <beneritter@gmail.com>
2016-03-29 07:28:04 +0200

    Externalize configuration for upload-dir

--- src/main/java/de/flux/playground/deckcompare/Deckcompare.java
+++ src/main/java/de/flux/playground/deckcompare/Deckcompare.java
@@ -1,11 +1,7 @@
 package de.flux.playground.deckcompare;
 
-import java.io.File;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,19 +10,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @SpringBootApplication
 public class Deckcompare {
 
-    public static String ROOT = "upload-dir";
-
     @RequestMapping("/")
     @ResponseBody
     public String home() {
         return "Hello World!";
-    }
-
-    @Bean
-    public CommandLineRunner init() {
-        return (String[] args) -> {
-            new File(ROOT).mkdir();
-        };
     }
 
     public static void main(String[] args) throws Exception {
