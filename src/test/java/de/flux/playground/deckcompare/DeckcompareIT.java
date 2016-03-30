8b5f4f91da9b6ef0206bc2e30647ecb0aaf8d957
d0gb0t <d0gb0t@web.de>
2016-03-31 00:19:37 +0200

    Add an integration test which checks if the application context loads without any errors.

---
+++ src/test/java/de/flux/playground/deckcompare/DeckcompareIT.java
@@ -0,0 +1,15 @@
+package de.flux.playground.deckcompare;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(Deckcompare.class)
+public class DeckcompareIT {
+
+	@Test
+	public void contextLoads() {
+	}
+}
