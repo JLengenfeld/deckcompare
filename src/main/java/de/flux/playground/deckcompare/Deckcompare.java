d2332686c380839b4acae2d0665124bc70ba29cf
JLengenfeld <jan.lengenfeld@gmail.com>
2016-04-03 23:15:19 +0200

    Remove unnecessary code

--- src/main/java/de/flux/playground/deckcompare/Deckcompare.java
+++ src/main/java/de/flux/playground/deckcompare/Deckcompare.java
@@ -2,19 +2,9 @@ package de.flux.playground.deckcompare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
 @SpringBootApplication
 public class Deckcompare {
-
-    @RequestMapping("/")
-    @ResponseBody
-    public String home() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Deckcompare.class, args);
